0f875cb1ef487259c69eda3bd8b378a79b2c7cfc
AW1270 <aoiwakabayashi0@outlook.jp>
2026-07-19 13:55:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,15 @@ $(function(){
     $('.hamberger_icon').toggleClass("active");
     $('body').toggleClass("active");
   });
+
+  $('a[href^="#"]').click(function () {
+    const speed = 600;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 });
 
 
